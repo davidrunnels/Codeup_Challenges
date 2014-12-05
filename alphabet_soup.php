94607464554f7b6ea984425627690863d7095ada
David Runnels <david.runnels@gmail.com>
2014-12-05 06:03:02 -0600

    refactor

--- alphabet_soup.php
+++ alphabet_soup.php
@@ -2,13 +2,21 @@
 
 
 function alphabet_soup($string) {
-	$word_array = str_split($string);
-	natcasesort($word_array);
-	$soup = implode($word_array);
+	$soup = '';
+	$word_array = explode(" ", $string);
+	
+	foreach($word_array as $word) {
+		$word = explode(' ', $word);
+		$letters = str_split($word[0]);
+		natcasesort($letters);
+		$broth = implode(' ', $letters);
+		$soup .= $broth . " ";
+		
+	}
 	return $soup;
 }
 
 echo "I'll make alphabet soup for you!\nGive me a list of words or a phrase: ";
-$word = (trim(fgets(STDIN)));
+$word = trim(fgets(STDIN));
 echo alphabet_soup($word);
 
